75249c3ecc4b0e3120048e8cae4058fcb0d357ee
andersonzach02 <andersonzach02@gmail.com>
2021-06-07 21:50:54 -0700

    Refactored implementation of the buttons and labels to use ttk rather than tk widgets. Additionally added an enter button for a future implementation for a way to enter a file's name without using the browse button.

--- application.py
+++ application.py
@@ -1,12 +1,14 @@
+from os import defpath
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
 
 class Application(tk.Frame): 
     def __init__(self, master=None):
         tk.Frame.__init__(self, master) 
         self.fileNames = []
         self.fileLabels = []
-        self.grid(sticky=tk.N+tk.S+tk.W+tk.E)
+        self.grid()
         self.createWidgets()
 
     def createWidgets(self):
@@ -15,14 +17,23 @@ class Application(tk.Frame):
         top.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
-        self.fileDialogButton = tk.Button(self, text='Browse',command=self.openFileExplorer) 
-        self.fileDialogButton.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
+
+        self.filePathEntry = ttk.Entry(self)
+        self.filePathEntry.grid(row=0, column=0)
+
+        self.enterButton = ttk.Button(self, text="Enter")
+        self.enterButton.grid(row=0, column=1)
+
+        self.fileDialogButton = ttk.Button(self, text='Browse',command=self.openFileExplorer) 
+        self.fileDialogButton.grid(row=0, column=2, columnspan=2, padx=5, pady=5)
+
 
     def openFileExplorer(self):
-        self.fileNames = filedialog.askopenfilenames(defaultextension=".pdf", filetypes={("*.pdf", ".pdf")})
-        print(self.fileNames)
-        for file in self.fileNames:
-            self.fileLabels.append(tk.Label(self, text=file).grid(row=1, column=0))
+        self.fileNames = tk.filedialog.askopenfilenames(defaultextension=".pdf", 
+                                                        filetypes={("*.pdf", ".pdf")})
+        for index in range(len(self.fileNames)):
+            currentFileName = self.fileNames[index]
+            self.fileLabels.append(ttk.Label(self, text=currentFileName).grid(row=index + 1, column=0))
 
 
 app = Application() 
